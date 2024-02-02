49a58742013a30c69dc35502faff3942d5718b7e
Bhavan663 <venkateshbhavankalyan@gmail.com>
2024-02-02 13:35:52 +0530

    pull panikoo da

--- vr/src/Hero/Images.jsx
+++ vr/src/Hero/Images.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+
 function Images() {
   return (
-    <div>
-nnnunun      
+    <div className='HeroMain' style={{display:"flex",justifyContent:"space-between"}} >
+        <div className='HeroContent'>
+            <h1>Elevate your Sport Performance</h1>
+        </div>
+        <div className='HeroImages'>
+            <img style={{ height: "550px" ,width: "530px",}} src="..\Set1.jpeg" alt="" />
+        </div>
     </div>
   )
 }
